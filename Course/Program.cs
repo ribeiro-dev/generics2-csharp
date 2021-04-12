fb8556ad888dfd9b63fc0679566c410f84bcbb0e
rafael <romilson.2r@gmail.com>
2021-04-12 16:54:44 -0300

    implement main program

--- Course/Program.cs
+++ Course/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Course.Entities;
+using Course.Services;
 
 namespace Course
 {
@@ -6,7 +10,27 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Product> list = new List<Product>();
+
+            Console.Write("Enter N: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] productData = Console.ReadLine().Split(',');
+                string name = productData[0];
+                double price = double.Parse(productData[1], CultureInfo.InvariantCulture);
+
+                list.Add(new Product(name, price));
+            }
+
+            CalculationService calculationService = new CalculationService();
+
+            Product p  = calculationService.Max(list);
+
+            Console.WriteLine();
+            Console.WriteLine("Most expensive: " + p);
+            
         }
     }
 }
